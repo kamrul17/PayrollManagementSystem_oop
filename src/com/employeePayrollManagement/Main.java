fd2724f793b89ac9d6b74fec862c7aa33b6c30ee
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-04-07 18:25:46 +0530

    second commit

--- src/com/employeePayrollManagement/Main.java
+++ src/com/employeePayrollManagement/Main.java
@@ -3,6 +3,7 @@ package com.employeePayrollManagement;
 public class Main {
     public static void main(String [] args){
 PayrollSystem pr=new PayrollSystem();
+        System.out.println("creating fulltym Employee obj-------");
 FullTimeEmp fe=new FullTimeEmp(1,"golu",1000.00);
 FullTimeEmp fe1=new FullTimeEmp(2,"golu3",12000.00);
 FullTimeEmp fe2=new FullTimeEmp(3,"golu4",10300.00);
